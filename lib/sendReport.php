a52024c5d67fa1a33b13d4bf97d9b5812b2de031
chriseverson <chris@chriseverson.net>
2014-06-24 10:56:07 -0700

    Email handling updates.

--- lib/sendReport.php
+++ lib/sendReport.php
@@ -1,5 +1,6 @@
 <?php
 require_once './helpers.php';
+require './PHPMailer/PHPMailerAutoload.php';
 require './Mustache/Autoloader.php';
 
 Mustache_Autoloader::register();
@@ -15,10 +16,33 @@ $aTemplateArgs = $_REQUEST;
 $aTemplateArgs['from_email'] = $SendParams['from_email'];
 $aTemplateArgs['from_name'] = $SendParams['from_name'] ? $SendParams['from_name'] : false;
 
-$subject = 'Support Details';
+// $subject = 'Support Details';
 
-$headers = "From: " . strip_tags($SendParams['from_email']) . "\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+// $headers = "From: " . strip_tags($SendParams['from_email']) . "\r\n";
+// $headers .= "MIME-Version: 1.0\r\n";
+// $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-mail('web@primergrey.com', $subject, $Template->render($aTemplateArgs), $headers);
+// mail('web@primergrey.com', $subject, $Template->render($aTemplateArgs), $headers);
+
+$Mail = new PHPMailer;
+
+$Mail->isSMTP();
+
+$Mail->Host       = 'smtp.gmail.com';
+$Mail->SMTPAuth   = true;
+$Mail->Username   = 'mailer@primergrey.com';
+$Mail->Password   = 'mailer2119';
+$Mail->SMTPSecure = 'tls';
+$Mail->Port       = 587;
+
+$Mail->setFrom($aTemplateArgs['from_email'], $aTemplateArgs['from_name']);
+$Mail->addAddress('web@primergrey.com');
+$Mail->isHTML(true);
+
+$Mail->Subject = 'New Browser Support Submission';
+$Mail->Body    = $Template->render($aTemplateArgs);
+
+if(!$Mail->send()) {
+   echo 'Mailer Error: ' . $Mail->ErrorInfo;
+   exit;
+}
